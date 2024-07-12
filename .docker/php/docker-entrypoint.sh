44a617f1960ab94e3673f00b18a8ee10a96c4b1a
Piotr Lachowicz <ptr.lachowicz@gmail.com>
2024-07-12 09:24:33 +0100

    modify script so that php container waits until db container is ready to receive connections

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 composer install
 
+while ! curl -o - symfony_dockerized_db_1:3306; do sleep 1; done
+
+echo "The db is now ready and reachable"
+
 bin/console doctrine:migrations:migrate --no-interaction
 
 exec php-fpm
